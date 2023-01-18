038512b85413afce88469e82eef2a0b916cf07a2
Qiqi ZHANG <qiqi@qiqis-mbp.wkstn.qmul.ac.uk>
2023-01-18 10:02:47 +0000

    3

--- test1.R
+++ test1.R
@@ -1,1 +1,10 @@
-aa=1
+f = function(x){
+  ((x+13)^2 + (5+65/x)^2 ) ^(1/2)
+}
+
+f(6.88)
+plot(sapply(seq(1,10,by=.01),f))
+optimise(f,interval = c(0,10))
+a = optimise(f,interval = c(0,10))$min
+
+polyroot(c(-65*130, -650, 0, 26, 2))
